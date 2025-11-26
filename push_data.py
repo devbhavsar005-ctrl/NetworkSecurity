565b28a8ba9e1b87823472c3db277ec2d89d3bd2
devbhavsar005-ctrl <devbhavsar005@gmail.com>
2025-11-26 22:49:26 +0530

    mongo_db data base is set

---
+++ push_data.py
@@ -0,0 +1,62 @@
+import os
+import sys
+import json
+
+from dotenv import load_dotenv
+load_dotenv()
+from NetworkSecurity.logging.logger import logging
+from NetworkSecurity.exception.exception import NetworkSecurityException
+
+MONGODB_URL=os.getenv("MONGODB_URL")
+print(MONGODB_URL)
+
+import certifi
+ca=certifi.where()
+
+import pandas as pd 
+import numpy as np 
+import pymongo
+
+class NetworkDataExtrat:
+    def __init__(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    def csv_to_json_convertor(self,file_path):
+        try:
+            data=pd.read_csv(file_path)
+            data.reset_index(drop=True,inplace=True)
+            records=list(json.loads(data.T.to_json()).values())
+            return records
+        
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)    
+        
+    def insert_data_mongodb(self,records,database,collection):
+        try:
+
+            self.records=records
+            self.database=database
+            self.collection=collection
+
+            self.mongo_client=pymongo.MongoClient(MONGODB_URL)
+            self.database = self.mongo_client[self.database]
+            self.collection=self.database[self.collection]
+            self.collection.insert_many(self.records)
+
+            return (len(self.records))
+        
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+if __name__=="__main__":
+    FILE_PATH=r"D:\NetworkSecurity\dataset\dataset.csv"
+    DATABASE="ML_preject-02"
+    COLLECTION="network security"
+    NetworkObject=NetworkDataExtrat()
+    records=NetworkObject.csv_to_json_convertor(file_path=FILE_PATH)
+    print(records)
+    no_of_records=NetworkObject.insert_data_mongodb(records,DATABASE,COLLECTION)
+    print(no_of_records)
